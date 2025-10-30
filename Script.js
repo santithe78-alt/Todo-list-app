e895ccbf077a4d9fc85d6433e9f63fd4de707fcf
santithe78-alt <santithe78@gmail.com>
2025-10-30 17:52:04 +0000

    Implemented add task

--- Script.js
+++ Script.js
@@ -1,1 +1,38 @@
-console.log("hello")
+console.log("hello");
+
+function addTask(event) {
+    //Get user in from text box
+    let textBox = document.getElementById("task-input");
+    let task = textBox.value;
+    
+    //create a list item
+    //Get To-do-list container div
+
+    let todolist = document.getElementById("to-do-list")
+
+
+    //create list-item div
+    let listDiv = document.createElement("div")
+    listDiv.id = "task" + todolist.childElementCount;
+    listDiv.classList.add("list-item");
+
+    //create checkbox
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = "checkbox" +todolist.childElementCount; 
+
+    //create label
+let label = document.createElement("label");
+    label.id = "label" + todolist.childElementCount;
+    label.innerText = task;
+
+    //Appeend checkbox to lost-item div
+    listDiv.appendChild(checkbox);
+
+
+    //Append label to div
+    listDiv.appendChild(label);
+
+    //Append list-item div to the list
+    todolist.appendChild(listDiv)
+}
